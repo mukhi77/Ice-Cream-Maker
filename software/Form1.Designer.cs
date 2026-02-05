c341ca9a3a7f154131fc5369afa6fca56dab156c
j graham <juliannagraham447@gmail.com>
2026-02-05 13:37:30 -0800

    software update for manual testing

---
+++ software/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace software
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblState = new System.Windows.Forms.Label();
+            this.lblSpeed = new System.Windows.Forms.Label();
+            this.lblTempNow = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.cboPorts = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxFileName = new System.Windows.Forms.TextBox();
+            this.buttonFileName = new System.Windows.Forms.Button();
+            this.checkBoxSave = new System.Windows.Forms.CheckBox();
+            this.trkSpeed = new System.Windows.Forms.TrackBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trkSpeed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(372, 95);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(4);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(341, 39);
+            this.btnStop.TabIndex = 30;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(21, 95);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(344, 39);
+            this.btnStart.TabIndex = 29;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblState
+            // 
+            this.lblState.AutoSize = true;
+            this.lblState.Location = new System.Drawing.Point(485, 52);
+            this.lblState.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblState.Name = "lblState";
+            this.lblState.Size = new System.Drawing.Size(86, 25);
+            this.lblState.TabIndex = 28;
+            this.lblState.Text = "State: 0";
+            // 
+            // lblSpeed
+            // 
+            this.lblSpeed.AutoSize = true;
+            this.lblSpeed.Location = new System.Drawing.Point(367, 52);
+            this.lblSpeed.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSpeed.Name = "lblSpeed";
+            this.lblSpeed.Size = new System.Drawing.Size(98, 25);
+            this.lblSpeed.TabIndex = 27;
+            this.lblSpeed.Text = "Speed: 0";
+            // 
+            // lblTempNow
+            // 
+            this.lblTempNow.AutoSize = true;
+            this.lblTempNow.Location = new System.Drawing.Point(277, 52);
+            this.lblTempNow.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTempNow.Name = "lblTempNow";
+            this.lblTempNow.Size = new System.Drawing.Size(54, 25);
+            this.lblTempNow.TabIndex = 23;
+            this.lblTempNow.Text = "36.5";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(16, 52);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(260, 25);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "Current Temperature (°C):";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(325, 5);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(4);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(387, 39);
+            this.btnConnect.TabIndex = 26;
+            this.btnConnect.Text = "Connect Serial";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // cboPorts
+            // 
+            this.cboPorts.FormattingEnabled = true;
+            this.cboPorts.Location = new System.Drawing.Point(139, 2);
+            this.cboPorts.Margin = new System.Windows.Forms.Padding(4);
+            this.cboPorts.Name = "cboPorts";
+            this.cboPorts.Size = new System.Drawing.Size(160, 33);
+            this.cboPorts.TabIndex = 25;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 25);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Serial Port:";
+            // 
+            // textBoxFileName
+            // 
+            this.textBoxFileName.Location = new System.Drawing.Point(21, 404);
+            this.textBoxFileName.Name = "textBoxFileName";
+            this.textBoxFileName.Size = new System.Drawing.Size(695, 31);
+            this.textBoxFileName.TabIndex = 57;
+            this.textBoxFileName.Text = "C:\\Users\\Julianna\\VS\\MECH421423\\MECH423ProjectV2\\datastream1.CSV";
+            // 
+            // buttonFileName
+            // 
+            this.buttonFileName.Location = new System.Drawing.Point(521, 339);
+            this.buttonFileName.Name = "buttonFileName";
+            this.buttonFileName.Size = new System.Drawing.Size(195, 50);
+            this.buttonFileName.TabIndex = 56;
+            this.buttonFileName.Text = "Select File Name";
+            this.buttonFileName.UseVisualStyleBackColor = true;
+            this.buttonFileName.MouseClick += new System.Windows.Forms.MouseEventHandler(this.buttonFileName_MouseClick);
+            // 
+            // checkBoxSave
+            // 
+            this.checkBoxSave.AutoSize = true;
+            this.checkBoxSave.Location = new System.Drawing.Point(21, 351);
+            this.checkBoxSave.Name = "checkBoxSave";
+            this.checkBoxSave.Size = new System.Drawing.Size(158, 29);
+            this.checkBoxSave.TabIndex = 55;
+            this.checkBoxSave.Text = "Save to File";
+            this.checkBoxSave.UseVisualStyleBackColor = true;
+            this.checkBoxSave.CheckedChanged += new System.EventHandler(this.checkBoxSave_CheckedChanged);
+            // 
+            // trkSpeed
+            // 
+            this.trkSpeed.AccessibleName = "";
+            this.trkSpeed.Location = new System.Drawing.Point(94, 151);
+            this.trkSpeed.Margin = new System.Windows.Forms.Padding(4);
+            this.trkSpeed.Maximum = 100;
+            this.trkSpeed.Minimum = -100;
+            this.trkSpeed.Name = "trkSpeed";
+            this.trkSpeed.Size = new System.Drawing.Size(540, 90);
+            this.trkSpeed.TabIndex = 58;
+            this.trkSpeed.Tag = "";
+            this.trkSpeed.TickFrequency = 10;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.IndianRed;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.IndianRed;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(21, 249);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(691, 51);
+            this.button1.TabIndex = 62;
+            this.button1.Text = "STOP";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(93, 202);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 25);
+            this.label2.TabIndex = 61;
+            this.label2.Text = "Max CCW";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(544, 202);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 25);
+            this.label3.TabIndex = 60;
+            this.label3.Text = "Max CW";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(309, 202);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 25);
+            this.label4.TabIndex = 59;
+            this.label4.Text = "Speed: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(728, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.trkSpeed);
+            this.Controls.Add(this.textBoxFileName);
+            this.Controls.Add(this.buttonFileName);
+            this.Controls.Add(this.checkBoxSave);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblState);
+            this.Controls.Add(this.lblSpeed);
+            this.Controls.Add(this.lblTempNow);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.cboPorts);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trkSpeed)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblState;
+        private System.Windows.Forms.Label lblSpeed;
+        private System.Windows.Forms.Label lblTempNow;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.ComboBox cboPorts;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxFileName;
+        private System.Windows.Forms.Button buttonFileName;
+        private System.Windows.Forms.CheckBox checkBoxSave;
+        private System.Windows.Forms.TrackBar trkSpeed;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ software/Form1.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace software
+{
+    public partial class Form1 : Form
+    {
+        SerialPort sp = new SerialPort();
+        StreamWriter outputFile;
+        DateTime startTime;
+        List<byte> buffer = new List<byte>();
+
+        // ==== Thermistor constants ====
+        const double VREF = 3.6;
+        const int ADC_MAX = 1023;
+        const double RS = 10000.0;
+        const double R0 = 6759.776536312849;
+        const double T0 = 309.35;
+        const double BETA = 3510.321997999228;
+        const double T_hi = 27.0;
+        const double T_lo = 23.0;
+        byte directionByte = 45;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeGUI();
+
+
+            // Fill COM list
+            cboPorts.Items.AddRange(SerialPort.GetPortNames());
+            if (cboPorts.Items.Count > 0) cboPorts.SelectedIndex = 0;
+
+            // Serial event handler
+            sp.DataReceived += Sp_DataReceived;
+        }
+
+        // ==== CONNECT / DISCONNECT HANDLER ====
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (!sp.IsOpen)
+            {
+                if (string.IsNullOrWhiteSpace(cboPorts.Text))
+                {
+                    MessageBox.Show("Please select a COM port.");
+                    return;
+                }
+
+                try
+                {
+                    sp.PortName = cboPorts.Text.Trim();
+                    sp.BaudRate = 9600;
+                    sp.DataBits = 8;
+                    sp.StopBits = StopBits.One;
+                    sp.Parity = Parity.None;
+                    sp.Handshake = Handshake.None;
+                    sp.ReadTimeout = 500;
+                    sp.WriteTimeout = 500;
+
+                    sp.Open();
+                    if (sp.IsOpen)
+                    {
+                        btnConnect.Text = "Disconnect Serial";
+                        startTime = DateTime.Now;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Port failed to open, sp.IsOpen = false");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Could not open port:\n" + ex.Message);
+                }
+            }
+            else
+            {
+                try
+                {
+                    sp.Close();
+                    btnConnect.Text = "Connect Serial";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error closing port:\n" + ex.Message);
+                }
+            }
+        }
+
+        // ==== Serial receive handler ====
+        private void Sp_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            int n = sp.BytesToRead;
+            byte[] temp = new byte[n];
+            sp.Read(temp, 0, n);
+            buffer.AddRange(temp);
+
+            while (buffer.Count >= 5)
+            {
+                if (buffer[0] != 0xFF)
+                {
+                    buffer.RemoveAt(0);
+                    continue;
+                }
+
+                byte tLo = buffer[1];
+                byte tHi = buffer[2];
+                byte speed = buffer[3];
+                byte state = buffer[4];
+                buffer.RemoveRange(0, 5);
+
+                short tempX100 = (short)(tLo | (tHi << 8));
+                double tempC = tempX100 / 100.0;
+
+                BeginInvoke(new Action(() =>
+                {
+                    lblTempNow.Text = $"{tempC:F2} °C";
+                    lblSpeed.Text = $"Speed: {speed}";
+                    lblState.Text = $"State: {StateName(state)}";
+
+
+                    if (checkBoxSave.Checked && outputFile != null)
+                    {
+                        string timeStamp = DateTime.Now.ToString("yyyy MMM dd HH:mm:ss");
+                        outputFile.WriteLine($"{tempC},{speed},{timeStamp}");
+                    }
+                }));
+            }
+        }
+
+        private double ApplyCompensation(double tempC)
+        {
+            double adj = (tempC);
+            return adj;
+        }
+
+        private byte lastSpeed = 255;
+
+        //private void btnStart_Click(object sender, EventArgs e)
+        //{
+        //    if (sp.IsOpen) sp.Write(new byte[] { (byte)'S' }, 0, 1);
+        //}
+
+        //private void btnStop_Click(object sender, EventArgs e)
+        //{
+        //    if (sp.IsOpen) sp.Write(new byte[] { (byte)'X' }, 0, 1);
+        //}
+
+        private string StateName(byte s)
+        {
+            // Must match firmware enum ordering (given below)
+            switch (s)
+            {
+                case 0: return "IDLE";
+                case 1: return "PRECOOL";
+                case 2: return "CHURN_RAMP";
+                case 3: return "CHURNING";
+                case 4: return "ANTI_JAM";
+                case 5: return "FINISH";
+                case 6: return "FAULT";
+                default: return $"UNKNOWN({s})";
+            }
+        }
+
+        private void InitializeGUI()
+        {
+            trkSpeed.Minimum = -170;
+            trkSpeed.Maximum = 170;
+            trkSpeed.Value = 0;
+
+            lblSpeed.Text = "Speed: 0";
+        }
+
+        private void UpdateSpeedLabel(byte speed)
+        {
+            if (lblSpeed.InvokeRequired)
+            {
+                lblSpeed.BeginInvoke(new Action(() =>
+                {
+                    lblSpeed.Text = $"Speed: {speed}";
+                }));
+            }
+            else
+            {
+                lblSpeed.Text = $"Speed: {speed}";
+            }
+        }
+
+        private void checkBoxSave_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxSave.Checked)
+            {
+                outputFile = new StreamWriter(textBoxFileName.Text);
+            }
+
+            else
+            {
+                outputFile.Flush();
+                outputFile.Close();
+                outputFile.Dispose();
+            }
+        }
+
+        private void buttonFileName_MouseClick(object sender, MouseEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Title = "Select a file name/location";
+            saveFileDialog.DefaultExt = "csv";
+            saveFileDialog.AddExtension = true;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBoxFileName.Text = saveFileDialog.FileName;
+            }
+        }
+
+        // ======================================
+        // SEND CONTINUOUS SPEED COMMAND (2 BYTES)
+        // ======================================
+        private void SendSpeedCommand(int value)
+        {
+            if (!sp.IsOpen)
+                return;
+
+            byte directionByte;
+            byte magnitudeByte;
+
+            if (value == 0)
+            {
+                directionByte = 43;      // '+'
+                magnitudeByte = 0;
+            }
+            else if (value > 0)
+            {
+                directionByte = 43;      // '+'
+                magnitudeByte = (byte)value;
+            }
+            else
+            {
+                directionByte = 45;      // '-'
+                magnitudeByte = (byte)(-value);
+            }
+
+            byte[] data = new byte[] { directionByte, magnitudeByte };
+            sp.Write(data, 0, 2);
+        }
+
+        // ======================================
+        // TRACKBAR MOVEMENT
+        // ======================================
+        private void trkSpeed_Scroll(object sender, EventArgs e)
+        {
+            int value = trkSpeed.Value;
+            lblSpeed.Text = "Speed: " + value;
+            SendSpeedCommand(value);
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            lblSpeed.Text = "Speed: " + 0;
+            trkSpeed.Value = 0;
+            SendSpeedCommand(0);
+        }
+    }
+}
